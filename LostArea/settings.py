a62e6cdcfff23854e377d8c95c2d5646d852aeb0
Vladimir <scherbinin.vladimir@gmail.com>
2019-12-22 12:28:37 +0300

    Готова основа приложения users. Логин, регистрация, создание профиля. Осталось наполнить профиль необходимыми полями.

--- LostArea/settings.py
+++ LostArea/settings.py
@@ -123,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = reverse_lazy('festival:index')
+LOGOUT_REDIRECT_URL = reverse_lazy('festival:index')
--- festival/views.py
+++ festival/views.py
@@ -5,5 +5,7 @@ class Index(TemplateView):
     template_name = 'index.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["username"] = self.request.user.username
         context["my_var"] = "my value"
         return context
